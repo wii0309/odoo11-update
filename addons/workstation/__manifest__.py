ca938eb300cc410cc01aa54b7ff9424d7f52979c
wii0309 <a2750612>
2018-09-04 09:12:52 +0800

    20180904 補

---
+++ addons/workstation/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "workstation",
+
+    'author': "NWEI_CRAFT",
+    'category': '',
+    'version': '1.0',
+    'description': """
+
+        """,
+    'depends': ['base','product'],
+
+    'data': [
+        'wizard/import_orders.xml',
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+}
